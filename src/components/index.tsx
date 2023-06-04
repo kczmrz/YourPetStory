84b66ef7d039cf05e056a2d5b4b8ea62734c8f37
Kczmrz <kczmrz@proton.me>
2023-06-04 19:59:43 +0200

    First configuration of about us page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export {default as Navbar} from './navbar/navbar';
 export {default as IndexPage} from './indexPage/indexpage';
+export {default as AboutUS} from './aboutPage/AboutPageComponent';
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,24 @@
+import { AboutUS } from "@/components"
+import { Box, useColorModeValue, useColorMode } from "@chakra-ui/react"
+import Head from "next/head";
+import { BgImage } from "@/images";
+
 export default function About()
 {
-    return(
-        <></>
+     /* Dzien / Noc - motyw */
+  const BoxBG = useColorModeValue(BgImage.day, BgImage.night);
+  const { colorMode, toggleColorMode } = useColorMode();
+ return(
+  <>
+    <Head>
+        <title>O nas!</title>
+    </Head>
+    <main className="">
+       <Box bgImage={BoxBG}  height={{lg: '100vh', sm: '150vh'}}> 
+           {/* ten komponent masz w folderze components i w pliku aboutpage/AboutPageComponent */}
+            <AboutUS/>
+       </Box>
+    </main>
+  </>
     )
 }
