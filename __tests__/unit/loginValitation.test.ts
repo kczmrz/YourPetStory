2bde392b1942259a46594bfd2e0f379bea0c9aaa
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-06-05 19:49:42 +0200

    Change validations for login and password, put it it one class

--- __tests__/unit/loginValitation.test.ts
+++ __tests__/unit/loginValitation.test.ts
@@ -1,34 +1,34 @@
 import { expect, test } from "@jest/globals";
-import { LoginValidation } from "../../src/utils/DatasValidation/LoginValidation";
+import { LoginAndPasswordValidation } from "../../src/utils/DatasValidation/LoginAndPasswordValidation";
 
 describe("Login validation,", () => {
   test("should return false when login length is short than 5", () => {
     const exampleLogin = "test";
 
-    expect(LoginValidation.validate(exampleLogin)).toBe(false);
+    expect(LoginAndPasswordValidation.validateLogin(exampleLogin)).toBe(false);
   });
 
   test("should return false when login doesn't contain special character", () => {
     const exampleLogin = "testing";
 
-    expect(LoginValidation.validate(exampleLogin)).toBe(false);
+    expect(LoginAndPasswordValidation.validateLogin(exampleLogin)).toBe(false);
   });
 
   test("should return false when login doesn't contain upper case character", () => {
     const exampleLogin = "testing!";
 
-    expect(LoginValidation.validate(exampleLogin)).toBe(false);
+    expect(LoginAndPasswordValidation.validateLogin(exampleLogin)).toBe(false);
   });
 
   test("should return false when login doesn't contain lower case character", () => {
     const exampleLogin = "TESTING!";
 
-    expect(LoginValidation.validate(exampleLogin)).toBe(false);
+    expect(LoginAndPasswordValidation.validateLogin(exampleLogin)).toBe(false);
   });
 
   test("should return true when login matches requirements", () => {
     const exampleLogin = "Testing!";
 
-    expect(LoginValidation.validate(exampleLogin)).toBe(true);
+    expect(LoginAndPasswordValidation.validateLogin(exampleLogin)).toBe(true);
   });
 });
