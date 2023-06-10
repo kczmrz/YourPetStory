55e1338871fd2663ed1ae8cfd8a8773df1ca4907
Kczmrz <kczmrz@proton.me>
2023-06-10 14:36:27 +0200

    Added a state of user

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import MyPets  from "../features/PetSlice";
 import Register  from "../features/registerSlice";
+import AppStateSlice  from "../features/appSlice";
 /*Główna konfiguracja stanu */
 export const store = configureStore({
    reducer: {
     MyPets: MyPets,   /*Dodanie już tego ogólnie do Reduxa */
-    Register: Register
+    Register: Register,
+    User: AppStateSlice,
    }
 })
 
