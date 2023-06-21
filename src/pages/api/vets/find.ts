64c8bec8d4ceb47a01039af8423592a892dceb8b
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-06-21 20:31:11 +0200

    Add scrapper method and functionality

--- src/pages/api/vets/find.ts
+++ src/pages/api/vets/find.ts
@@ -7,29 +7,56 @@ export default async function handler(
 ) {
   const vetsDatabaseLink = "https://www.znanylekarz.pl/weterynarz/";
 
-  try {
-    const { keyword } = req.query;
-    if (!keyword) {
-      return res.status(400).json({ error: "Keyword is missing" });
+  if (req.method === "GET") {
+    try {
+      const { keyword } = req.query;
+      if (!keyword) {
+        return res.status(400).json({ error: "Keyword is missing" });
+      }
+      const browser = await puppeteer.launch({
+        defaultViewport: {
+          width: 1920,
+          height: 1080,
+        },
+        args: ["--incognito"],
+        headless: false,
+      });
+      const page = await browser.newPage();
+      await page.goto(vetsDatabaseLink + keyword);
+      await page.waitForSelector("#onetrust-accept-btn-handler");
+      await page.click("#onetrust-accept-btn-handler");
+
+      // const vetOffers = document.querySelectorAll("#search-content > ul > li");
+      const offers = await page.$$("#search-content > ul > li");
+      const offersAmount = offers.length;
+
+      for (let i = 1; i < offersAmount; i++) {
+        console.log(i);
+        const name = await page.$eval(
+          `#search-content > ul > li:nth-child(${i}) div.card-body div.result-column .dp-doctor-card .media .media-body h3 a`,
+          (element) => element.innerText
+        );
+
+        const address = await page.$eval(
+          `#search-content > ul > li:nth-child(${i}) div.card-body div.result-column .doctor-card-address div:nth-child(2) .align-items-top .overflow-hidden p span`,
+          (element) => element.innerText
+        );
+
+        const imageLink = await page.$eval(
+          `#search-content > ul > li:nth-child(${i}) div div div div.col-lg-6.result-column.p-2 div.dp-doctor-card.dp-doctor-card-md div div.pr-1 a span img`,
+          (element) => element.src
+        );
+
+        console.log(name, address, imageLink);
+      }
+
+      await browser.close();
+
+      res.status(200).json({ test: 123 });
+    } catch (error) {
+      res.status(500).json({ error: "An error occurred" });
     }
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(vetsDatabaseLink + keyword);
-
-    const data = await page.evaluate(() => {
-      const vetOffers = document.querySelectorAll("#search-content > ul > li");
-
-      // const vetsDatas = vetOffers.map((li) => {
-
-      // })
-
-      return { vetOffers };
-    });
-
-    await browser.close();
-
-    res.status(200).json(data);
-  } catch (error) {
+  } else {
     res.status(500).json({ error: "An error occurred" });
   }
 }
